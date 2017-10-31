4e9e28b86b06fe095ab845380ab5387b99833527
d-velev <bamzorlol@gmail.com>
2017-10-31 15:26:16 +0200

    [GH-#88] Fixed incorrect get_block_by_hash calls

--- apps/aecore/test/aecore_chain_test.exs
+++ apps/aecore/test/aecore_chain_test.exs
@@ -43,7 +43,8 @@ defmodule AecoreChainTest do
     latest_block_hash_hex = latest_block_hash |> Base.encode16()
     [latest_block | [previous_block | []]] = Chain.get_blocks(latest_block_hash, 2)
 
-    assert latest_block == Chain.get_block_by_hash(latest_block_hash_hex)
+    assert latest_block == Chain.get_block_by_hash(latest_block_hash, :bin)
+    assert latest_block == Chain.get_block_by_hash(latest_block_hash_hex, :hex)
     assert previous_block.header.height + 1 == latest_block.header.height
     assert BlockValidation.validate_block!(latest_block, previous_block, Chain.chain_state())
     assert :ok = Chain.add_block(block)
